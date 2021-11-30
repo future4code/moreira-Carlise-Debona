b4712c9df1c94f1225639f5121e6a9cb9b4adbfe
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2021-11-30 15:39:04 -0300

    entreda da atividade sobre operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,56 @@
+//1. a. false
+// b. false
+// c. true
+// d. boolean
+
+//2. O problema é que não está convertendo a string em número. No console vai imprimir a junção dos dois números, pois está concatenando.
+
+// 3.
+//let primeiroNumero = Number(prompt("Digite um numero!"));
+//let segundoNumero = Number10(prompt("Digite outro numero!"));
+
+//const soma = primeiroNumero + segundoNumero;
+
+//console.log(soma);
+
+//Exercícios de escrita de código
+//1.
+
+//let suaIdade = Number(prompt("Qual a sua idade?"));
+//let idadeAmigo = Number(prompt("Idade do seu melhor amigo(a)?"));
+//let diferencaIdade = suaIdade - idadeAmigo;
+
+//console.log( "Sua idade é maior do que a do seu melhor amigo?",   suaIdade > idadeAmigo, "Diferença de idade:", diferencaIdade);
+
+//2.
+
+//let numeroPar = Number(prompt("Insira um número par?"));
+
+//let restoDaDivisao = numeroPar % 2;
+
+//console.log(restoDaDivisao);
+
+// c) Com número par o resto da divisão sempre será zero.
+// d) Se usaro um númer impar o resto da divisão nunca será zero.
+
+//3.
+//let idadeEmAnos = Number(prompt(" a) Qual sua idade?"));
+
+//let idadeMeses = idadeEmAnos * 12;
+
+//let idadeDias = idadeEmAnos * 365;
+
+//let idadeHoras = idadeEmAnos * 8760;
+
+//console.log("a) A idade do usuário em meses", idadeMeses);
+//console.log("b) A idade do usuário em dias", idadeDias);
+//console.log("c) A idade do usuário em horas", idadeHoras);
+
+//4.
+//let primeiroNumero = Number(prompt("Digite um número?"));
+//let segundoNumero = Number(prompt("Digite outro número?"));
+//let divisivel1 = primeiroNumero % segundoNumero;
+//let resultadoDivisivel1 = console.log( "O primeiro numero é maior que segundo?", primeiroNumero > segundoNumero);
+//console.log( "O primeiro numero é igual que segundo?", primeiroNumero === segundoNumero);
+//console.log("O primeiro numero é divisível que segundo?", primeiroNumero == segundoNumero);
+//console.log("O segundo numero é divisível que primeiro?", segundoNumero == primeiroNumero);
