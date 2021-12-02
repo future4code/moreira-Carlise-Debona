674b4b6f9eae6b3b6fd86ab57da8f59027e6265e
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2021-12-02 17:07:53 -0300

    trabalho de funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,88 @@
+//1. 
+// a) 10 
+//    50
+
+// b) ele faz o calculo, mas não aparece no console os valores, fica em branco.
+
+//2. 
+// a) O código solicita um texto, depois foi feito uma função que deixa o texto com letras minúsculas e 
+// e também a função procura a palavra cenoura para dizer se é true ou false. Quando é solicitada a função, 
+// ela passa sua ação para o texto digitado, e é imprimdo no console true ou false que significa 
+// se encontrou ou não cenoura.
+
+// b) i. true
+//    ii. true
+//    iii. true
+
+// Exercícios de escrita de código
+
+// a) 
+
+
+// let texto= function() {
+// console.log('Eu sou Carlise, tenho 32 anos, moro em Porto Alegre e sou estudante.')
+
+// }
+
+// texto()
+
+
+// b) 
+
+// const frase = function(nome,idade, cidade, profissao){
+//   const nomeUsuario = "Carlise"
+//   const idadeUsuario = "32"
+//   const cidadeUsuario = "Porto Alegre"
+//   const profissaoUsuario = "Programadora"
+        
+//   console.log(`Eu sou ${nomeUsuario}, tenho ${idadeUsuario} anos, moro em ${cidadeUsuario} e sou ${profissaoUsuario}.` )
+
+// }
+
+// frase()
+
+//2.
+
+// a)
+
+//let soma = function(valor1,valor2){
+
+// let resultado = valor1 + valor2
+// console.log(resultado)
+
+// }
+
+// soma(9,20)
+
+//b)
+
+// let booleano = function(valor1,valor2){
+
+//   let resultado = valor1 >= valor2
+//   console.log(resultado)
+  
+//   }
+  
+//   booleano (100,20)
+
+//c)
+
+// let parOuImpar = function(valor){
+
+// let resto = valor % 2 
+// let par = resto === 0
+// console.log(par)
+
+//   }
+    
+// parOuImpar(20)
+
+// d. 
+
+const frase = function (texto){
+let nome = ("Bom dia meus amigos!")
+let nomeMaiusculo = nome.length().toUpperCase()
+console.log(nomeMaiusculo)    
+}
+
+frase()
