89343c461aa78b49b5defe56e381228c10467799
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2021-12-06 20:00:57 -0300

    Update index.js

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -1,1 +1,85 @@
-//1. 
+//1.
+// a) 
+// Matheus
+// Virginia Cavendish
+// Globo 14h
+
+//2.
+// a) nome:Juca idade:3 racaSRD
+//    nome:Juba idade:3 racaSRD
+//    nome:Jubo idade:3 racaSRD
+// b) spred 
+
+//3.
+// a) false 
+//    undefined
+
+// b) No objeto não tem a chave altura.
+
+//Exercícios de escrita de código
+
+//1.
+// a)
+// const pessoas = {
+// nome: "Gustavo",
+// apelidos: ["Gu", "Guga", "Guto"]
+// }
+
+// novoApelido = {
+// ... pessoas,
+// apelidos: ["Gutinho", "Guguinho", "Gutinho"]
+// }
+
+// function outrosApelidos(nomes) {
+
+// console.log(`Olá, me chamo ${nomes.nome}, mas você pode me chamar por um desses apelidos:
+// ${nomes.apelidos[0]} ou ${nomes.apelidos[1]} ou ${nomes.apelidos[2]}.`)
+
+// }
+
+// outrosApelidos(novoApelido)
+
+//2.
+// a)
+// const pessoas = {
+// nome: "Marli",
+// idade: 28,
+// profissao: "programador"
+// }
+
+// const nome = {
+// nome: "Nicolas",
+// idade: 18,
+// profissao: "Jogador de Futebol"
+
+// }
+
+// function informacaoPessoas(dadosDasPessoas) {
+
+// let informacaoDasPessoas = [pessoas.nome, pessoas.nome.length, pessoas.idade, pessoas.profissao, pessoas.profissao.length]
+// let informacaoDasPessoas2 = [nome.nome, nome.nome.length, nome.idade, nome.profissao, nome.profissao.length]
+
+// console.log(informacaoDasPessoas) 
+// console.log(informacaoDasPessoas2)
+
+// }
+
+// informacaoPessoas(pessoas, nome)
+
+//3. 
+// const carrinho = []
+
+// const tiposDeFrutas = [
+// {nome: "Morango", disponibilidade: true},
+// {nome: "Uva", disponibilidade: false},
+// {nome: "Pessêgo", disponibilidade: true} 
+// ]
+
+// function listaDeFrutas(frutas){
+
+// return carrinho.push(frutas)
+
+// }
+
+// listaDeFrutas(tiposDeFrutas)
+// console.log(carrinho)
