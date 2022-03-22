6b3cc2709b854b28f35726568333002eba2124e6
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-03-22 19:17:10 -0300

    atividade node

---
+++ modulo4/node-package-json/nomeIdade/index.js
@@ -0,0 +1,15 @@
+//1. com os scripts.
+
+const nome =(process.argv[2])
+const idade = Number(process.argv[3])
+const frase = `Ola ${nome} voce tem ${idade} ano`
+const novaIdade = (idade +7) 
+const sete = `Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}`
+console.log(frase)
+console.log(sete)
+
+
+
+
+
+
