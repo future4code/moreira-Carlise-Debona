b13c865de19d4110bab119f0bfe70bb7271573ec
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-04-14 21:44:04 -0300

    Buscar todos os usuários

--- modulo4/projeto-labecommerce_users/src/index.ts
+++ modulo4/projeto-labecommerce_users/src/index.ts
@@ -1,6 +1,7 @@
 import { app } from "./app"
-
+import {getAllAUsers} from './endepoints/getAllUsers';
 import { createUsers }  from './endepoints/createUsers';
 
 
 app.post('/users', createUsers);
+app.get('/users',getAllAUsers);
